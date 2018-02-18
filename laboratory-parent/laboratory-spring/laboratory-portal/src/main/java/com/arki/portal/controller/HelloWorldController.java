ea72b6dbe702cc12c41c1f49f199f3b318efd7e6
ArkiZh <youneverknowai@foxmail.com>
2018-02-18 21:46:14 +0800

    Doing.

--- laboratory-parent/laboratory-spring/laboratory-portal/src/main/java/com/arki/portal/controller/HelloWorldController.java
+++ laboratory-parent/laboratory-spring/laboratory-portal/src/main/java/com/arki/portal/controller/HelloWorldController.java
@@ -1,10 +1,12 @@
 package com.arki.portal.controller;
 
 
+import com.arki.laboratory.common.Logger;
 import com.arki.portal.service.HelloWorldService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -14,9 +16,10 @@ public class HelloWorldController {
     @Autowired
     HelloWorldService helloWorldService;
 
-    @RequestMapping("/hello")
+    @RequestMapping(value = "/hello",method = RequestMethod.GET)
     @ResponseBody
     public String hello(String name) {
+        Logger.info("Parameter: name=[{}]", name);
         String result = this.helloWorldService.hello(name);
         return result;
     }
