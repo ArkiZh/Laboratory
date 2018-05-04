8a1d323d2da1b7245e75cc74aa866f78db2d201e
ArkiZh <youneverknowai@foxmail.com>
2018-05-04 15:40:12 +0800

    Test SecureRandom.

---
+++ laboratory-parent/laboratory-snippet/src/main/java/com/arki/laboratory/snippet/SecureRandomTest.java
@@ -0,0 +1,32 @@
+package com.arki.laboratory.snippet;
+
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+
+public class SecureRandomTest {
+    public static void main(String[] args) throws NoSuchAlgorithmException {
+        String s = generateRandomLetterString(60);
+        System.out.println(s);
+    }
+
+    /**
+     * Generate a length-specified letter string by SecureRandom.
+     * @param length
+     * @return
+     */
+    public static String generateRandomLetterString(int length) {
+        String letterStr = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        SecureRandom random;
+        try {
+            random = SecureRandom.getInstance("SHA1PRNG");
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+            return null;
+        }
+        StringBuilder sb = new StringBuilder(length);
+        for (int i = 0; i < length; i++) {
+            sb.append(letterStr.charAt(random.nextInt(52)));
+        }
+        return sb.toString();
+    }
+}
