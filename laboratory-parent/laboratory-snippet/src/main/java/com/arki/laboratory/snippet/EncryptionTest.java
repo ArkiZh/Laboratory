f64e58babbe448657aa88b2659fb209399bc614a
ArkiZh <youneverknowai@foxmail.com>
2018-05-04 10:44:42 +0800

    Test encription: HMAC.

--- laboratory-parent/laboratory-snippet/src/main/java/com/arki/laboratory/snippet/EncryptionTest.java
+++ laboratory-parent/laboratory-snippet/src/main/java/com/arki/laboratory/snippet/EncryptionTest.java
@@ -25,10 +25,25 @@ public class EncryptionTest {
     public static final String HMAC_SHA512 = "HmacSHA512";
 
 
-//    public static void main(String[] args) {
-//        String result = macCalculate("Zhang", HMAC_SHA256, "love");
-//        System.out.println(result);
-//    }
+    public static void main(String[] args) {
+        System.out.println("=============== Test calculating mac(message authentication code):");
+        String macType = HMAC_SHA256;
+        String key = "HelloKey!";
+        String message = "HelloWorld!";
+        String macResult = macCalculate(message, macType, key);
+        System.out.println("Mac algorithm: "+macType);
+        System.out.println("Secret key: " + key);
+        System.out.println("Message: " + message);
+        System.out.println("Mac: " + macResult);
+
+        System.out.println("=============== Test generating secret key:");
+        byte[] bytes = generateSecretKey(macType);
+        System.out.println("Mac algorithm: " + macType);
+        System.out.println("Generate secret key: " + convertByteArrayToString(bytes));
+        String hexStr = byteArrayToHexStringByMappingChar(bytes);
+        System.out.println("Convert bytes to hex string: " + hexStr);
+        System.out.println("Convert hex string back to bytes: " + convertByteArrayToString(hexStringToByteArray(hexStr)));
+    }
 
     /**
      * Generate secret key for the specified mac type.
@@ -60,7 +75,7 @@ public class EncryptionTest {
             Mac mac = Mac.getInstance(macType);
             mac.init(key);
             byte[] bytes = mac.doFinal(message.getBytes());
-            String s = byteArrayToHexString(bytes);
+            String s = byteArrayToHexStringByJdk(bytes);
             return s;
         } catch (NoSuchAlgorithmException | InvalidKeyException e) {
             e.printStackTrace();
@@ -69,24 +84,30 @@ public class EncryptionTest {
     }
 
     /**
-     * Transfer byte array to hex string.
+     * Convert byte array to hex string.
      *
      * @param bytes
      * @return
      */
-    private static String byteArrayToHexString(byte[] bytes) {
+    private static String byteArrayToHexStringByJdk(byte[] bytes) {
         StringBuilder sb = new StringBuilder();
         for (int n = 0; bytes != null && n < bytes.length; n++) {
+            // '&0xFF' to ensure the lower 8 bits not be changed when negative byte is converted to integer.
             String temp = Integer.toHexString(bytes[n] & 0XFF);
-            System.out.println(bytes[n] +" --> " + temp);
-            if (temp.length() == 1)
-                sb.append('0');
+            // If the length of hex string is 1, append an extra "0" before {temp},
+            // to ensure each byte be converted to a string length of 2.
+            if (temp.length() == 1) sb.append('0');
             sb.append(temp);
         }
         return sb.toString();
     }
 
-    private static String byteArrayToHexString1(byte[] bytes) {
+    /**
+     * Convert byte array to hex string.
+     * @param bytes
+     * @return
+     */
+    private static String byteArrayToHexStringByMappingChar(byte[] bytes) {
         char[] hexCode = "0123456789abcdef".toCharArray();
         char[] hexChars = new char[bytes.length * 2];
         for (int i = 0; i < bytes.length; i++) {
@@ -97,13 +118,38 @@ public class EncryptionTest {
         return new String(hexChars);
     }
 
-    public static void main(String[] args) {
-        byte[] bytes = {-128,-2, -1, 0, 1, 2,127};
-        String s = byteArrayToHexString(bytes);
-        System.out.println(s);
-        String s1 = byteArrayToHexString1(bytes);
-        System.out.println(s1);
-
+    /**
+     * Contert hex string to byte array.
+     * @param hexString
+     * @return
+     */
+    private static byte[] hexStringToByteArray(String hexString) {
+        if (hexString == null || hexString.length() % 2 != 0) {
+            return null;
+        }
+        byte[] bytes = new byte[hexString.length() / 2];
+        String lowerCase = hexString.toLowerCase();
+        String hexBaseStr = "0123456789abcdef";
+        for (int i = 0; i < lowerCase.length(); i += 2) {
+            // Convert the higher 4 bits to integer.
+            char c1 = lowerCase.charAt(i);
+            int int1 = hexBaseStr.indexOf(c1);
+            // Convert the lower 4 bits to integer.
+            char c2 = lowerCase.charAt(i + 1);
+            int int2 = hexBaseStr.indexOf(c2);
+            // Combine the higher 4 bits and lower 4 bits to a byte.
+            bytes[i / 2] = (byte) (((int1 << 4) | (int2 & 0xF)) & 0xFF);
+        }
+        return bytes;
     }
 
+    private static String convertByteArrayToString(byte[] bytes){
+        StringBuilder sb = new StringBuilder();
+        sb.append('[');
+        for (int i = 0; i < bytes.length; i++) {
+            sb.append(bytes[i]).append(',');
+        }
+        String s = sb.substring(0, sb.length() - 1);
+        return s + "]";
+    }
 }
