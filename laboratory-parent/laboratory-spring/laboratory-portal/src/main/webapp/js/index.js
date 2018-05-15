8710f5a93f5fba2d60844641cddc3ca01bf6e8e5
ArkiZh <youneverknowai@foxmail.com>
2018-05-15 10:54:53 +0800

    Test webapp.

--- laboratory-parent/laboratory-spring/laboratory-portal/src/main/webapp/js/index.js
+++ laboratory-parent/laboratory-spring/laboratory-portal/src/main/webapp/js/index.js
@@ -1,7 +1,7 @@
 $(function(){
     $("#echoButton").click(function(){
         $.ajax({
-            url : "http://localhost:8080/helloworld/hello",
+            url : "/helloworld/hello",
             type: "GET",
             data: {name:$("#name").val()},
             success: function (message) {
