a613a26328796afd80febf3289438b5546251ebd
ArkiZh <1797466468@qq.com>
2018-11-28 15:35:21 +0800

    Doing.

--- laboratory-parent/laboratory-snippet/src/main/java/com/arki/laboratory/snippet/MyTest.java
+++ laboratory-parent/laboratory-snippet/src/main/java/com/arki/laboratory/snippet/MyTest.java
@@ -153,14 +153,14 @@ public class MyTest {
 
 	@Test
 	public void testUrlCode(){
-		String encode = URLEncoder.encode("http://localhost:8089/para-server/sw/base/para/depParaList?paraName=MOC_NALI_COUNTRY_CODE&filterCon=cny_cde<>EU,cny_cde!#0%&rowNum=5000");
+		String encode = URLEncoder.encode("http://localhost:8089/xxx?XXX=XXX&XXX=XXX<>XXX,XXX!#0%&XXX=5000");
 		System.out.println(encode);
 		System.out.println(URLEncoder.encode("#")); //%23
 		System.out.println(URLEncoder.encode("%")); //%25
 		System.out.println(URLEncoder.encode("!")); //%21
 		System.out.println(URLEncoder.encode("@")); //%40
 		System.out.println("".split(",").length);
-		String decode = URLDecoder.decode("[route%3A%2F%2F0.0.0.0%2Fcom.sw.plat.para.api.service.ISwParaApiService%3Fcategory%3Drouters%26dynamic%3Dfalse%26enabled%3Dtrue%26force%3Dtrue%26group%3DLS%26name%3DLS%2Fcom.sw.plat.para.api.service.ISwParaApiService+blackwhitelist%26priority%3D0%26router%3Dcondition%26rule%3Dconsumer.host%2B%2521%253D%2B192.168.0.79%252C192.168.2.32%252C192.168.10.1%252C192.168.59.1%252C192.168.0.72%252C192.168.3.36%252C192.168.40.1%252C192.168.2.76%252C192.168.3.168%252C192.168.2.125%252C192.168.3.122%252C192.168.100.55%252C192.168.81.1%252C192.168.2.129%252C192.168.3.73%252C192.168.220.1%252C192.168.3.70%252C192.168.3.128%252C192.168.0.83%252C10.100.100.33%252C10.100.100.5%252C192.168.3.246%252C10.100.100.71%252C10.0.0.33%252C192.168.136.1%252C192.168.3.133%252C192.168.2.3%252C192.168.2.134%252C192.168.190.1%252C192.168.1.91%252C192.168.3.107%252C10.0.0.20%252C10.0.0.4%252C192.168.2.11%252C10.0.0.68%252C10.0.0.2%252C192.168.3.99%252C192.168.0.21%252C192.168.2.149%252C192.168.70.1%252C192.168.2.92%252C192.168.0.63%252C192.168.2.102%252C192.168.1.226%252C10.0.0.14%252C192.168.1.227%252C192.168.2.62%252C192.168.100.176%252C192.168.1.224%252C10.100.100.12%252C192.168.1.225%252C192.168.2.218%252C10.0.0.57%252C192.168.1.87%252C192.168.1.223%252C10.0.0.53%252C192.168.3.152%252C192.168.2.111%252C192.168.3.150%252C192.168.0.53%252C10.0.0.112%252C192.168.2.181%2B%253D%253E%2Bfalse%26runtime%3Dfalse%26type%3Djavascript]");
+		String decode = URLDecoder.decode("[route%3A%2F%2F0.0.0.0%2Fcom.xxxService%3Fcategory%3Drouters%26dynamic%3Dfalse%26enabled%3Dtrue%26force%3Dtrue%26group%3DLS%26name%3DLS%2Fcom.xxxService+blackwhitelist%26priority%3D0%26router%3Dcondition%26rule%3Dconsumer.host%2B%2521%253D%2B192.168.0.0%2B%253D%253E%2Bfalse%26runtime%3Dfalse%26type%3Djavascript]");
 		System.out.println(URLDecoder.decode(decode));
 	}
 }
