469b685f1791f48937d465798eebe628c73d7902
ArkiZh <1797466468@qq.com>
2018-12-24 16:34:04 +0800

    Doing. Test spring cloud sleuth.

--- laboratory-springbootbased/laboratory-ribbonconsumer/src/main/java/com/arki/laboratory/ribbonconsumer/web/ConsumerSideController.java
+++ laboratory-springbootbased/laboratory-ribbonconsumer/src/main/java/com/arki/laboratory/ribbonconsumer/web/ConsumerSideController.java
@@ -1,6 +1,10 @@
 package com.arki.laboratory.ribbonconsumer.web;
 
+import com.arki.laboratory.ribbonconsumer.sampler.MyAlwaysSampler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,12 +15,19 @@ import org.springframework.web.client.RestTemplate;
 @RestController
 public class ConsumerSideController {
 
-
     @Autowired
     private RestTemplate restTemplate;
 
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Bean
+    public MyAlwaysSampler defaultSampler() {
+        return new MyAlwaysSampler(true);
+    }
+
     @RequestMapping("/consume/{serviceName}/{methodName}")
     public String consumeTargetService(@PathVariable String serviceName,@PathVariable String methodName) {
+        logger.info("Method invoked: [{}]", "consumeTargetService");
         ResponseEntity<String> entity = restTemplate.getForEntity("http://" + serviceName + "/" + methodName, String.class);
         String body = entity.getBody();
         return body;
--- laboratory-springbootbased/laboratory-serviceprovider/src/main/java/com/arki/laboratory/serviceprovider/web/HelloController.java
+++ laboratory-springbootbased/laboratory-serviceprovider/src/main/java/com/arki/laboratory/serviceprovider/web/HelloController.java
@@ -2,19 +2,25 @@ package com.arki.laboratory.serviceprovider.web;
 
 import com.arki.laboratory.serviceprovider.util.SpringUtil;
 import com.arki.laboratory.serviceprovider.bo.ApplicationInfo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
     @RequestMapping("/hello")
     public String hello(){
+        logger.info("Method invoked: [{}]", "hello");
         return "Hello world! From: " + getAppInfo();
     }
 
     @RequestMapping("/appinfo")
     public ApplicationInfo getAppInfo() {
+        logger.info("Method invoked: [{}]", "getAppInfo");
         return SpringUtil.getBean(ApplicationInfo.class);
     }
 }
